e093ed6d4458d30ece7618f2a464a6c69ce5fb7e
Audrius Rackauskas <audrius@horiondigital.com>
2016-09-09 21:31:21 +0300

    removed comments

--- acf-front-end-editor.php
+++ acf-front-end-editor.php
@@ -1,13 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
  * @link              http://www.horiondigital.com
  * @since             1.0.0
  * @package           Acf_Front_End_Editor
